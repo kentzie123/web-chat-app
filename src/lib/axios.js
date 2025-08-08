544c5bf310e3112936ab1c60dc7a1aede857c8e5
Kent Adriane Goc-ong <kentadriane2@gmail.com>
2025-08-09 00:05:20 +0800

    feat: update API base URL in axios configuration to include '/api' for correct endpoint access

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, // Change to http://localhost:5000/api if using localhost
+  baseURL: `${import.meta.env.VITE_API_URL}/api`, // Change to http://localhost:5000/api if using localhost
   withCredentials: true,
 });
 
