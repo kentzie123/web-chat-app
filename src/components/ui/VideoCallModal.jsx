5230af8fdca081473addbe1b33674e35d2d091dd
Kent Adriane Goc-ong <kentadriane2@gmail.com>
2025-08-10 14:08:23 +0800

    changes on vidoecalling mic and cam

--- src/components/ui/VideoCallModal.jsx
+++ src/components/ui/VideoCallModal.jsx
@@ -1,35 +1,78 @@
 // Lucide icons
-import { Mic, MicOff, Video, VideoOff, PhoneOff  } from 'lucide-react';
+import { Mic, MicOff, Video, VideoOff, PhoneOff } from "lucide-react";
 
 // Hooks
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef, useEffect } from "react";
 
 const VideoCallModal = () => {
   const myVideoRef = useRef(null);
   const [isMicOn, setIsMicOn] = useState(true);
   const [isCamOn, setIsCamOn] = useState(true);
 
-  const startStream = async () => {
-    const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
-    myVideoRef.current.srcObject = stream;
-    // stream.getTracks().forEach(track => pcRef.current.addTrack(track, stream));
-  };
-
-  useEffect(()=>{
+  useEffect(() => {
+    const startStream = async () => {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+          video: isCamOn,
+          audio: isMicOn,
+        });
+        if (myVideoRef.current) {
+          myVideoRef.current.srcObject = stream;
+          // stream.getTracks().forEach(track => pcRef.current.addTrack(track, stream));
+        }
+      } catch (error) {
+        console.error("Error starting camera:", error);
+      }
+    };
     startStream();
-  },[])
-  
+  }, [isCamOn, isMicOn]);
 
   return (
     <div className="fixed w-screen h-screen flex justify-center items-center bg-black/80 z-100">
       {/* <video src="" className="scale-x-[-1] w-full max-w-[500px]" autoPlay playsInline/> */}
-      <div className="relative h-[60%] w-full max-w-[800px]">
-         <video ref={myVideoRef} className="w-full h-full bg-white rounded-lg" autoPlay playsInline></video>  {/*my camera */}
-        <div className="absolute bottom-3 right-1 rounded-lg border-2 border-primary bg-base-100 h-35 w-50"></div>  {/*other user camera */}
+      <div className="relative h-[60%] w-full max-w-[800px] bg-black">
+        <video
+          ref={myVideoRef}
+          className="w-full h-full rounded-lg scale-x-[-1]"
+          autoPlay
+          playsInline
+        ></video>{" "}
+        {/*my camera */}
+        <div className="absolute bottom-3 right-1 rounded-lg border-2 border-primary bg-base-100 h-35 w-50"></div>{" "}
+        {/*other user camera */}
         <div className="absolute bottom-3 left-1/2 -translate-x-1/2 flex gap-3 p-2 rounded-full bg-gray-900/60">
-          <button onClick={()=> setIsMicOn(prev => !prev)} type="button" className={`btn ${!isMicOn ? 'btn-error' : ''} btn-circle rounded-full`}>{isMicOn ? <Mic className='size-4'/> : <MicOff className='size-4'/>}</button>
-          <button onClick={()=> setIsCamOn(prev => !prev)} type="button" className={`btn ${!isCamOn ? 'btn-error' : ''} btn-circle rounded-full`}>{isCamOn ? <Video className='size-4'/> : <VideoOff className='size-4'/>}</button>
-          <button type="button" className="btn btn-error btn-circle rounded-full"><PhoneOff className='size-4'/></button>
+          <button
+            onClick={() => setIsMicOn((prev) => !prev)}
+            type="button"
+            className={`btn ${
+              !isMicOn ? "btn-error" : ""
+            } btn-circle rounded-full`}
+          >
+            {isMicOn ? (
+              <Mic className="size-4" />
+            ) : (
+              <MicOff className="size-4" />
+            )}
+          </button>
+          <button
+            onClick={() => setIsCamOn((prev) => !prev)}
+            type="button"
+            className={`btn ${
+              !isCamOn ? "btn-error" : ""
+            } btn-circle rounded-full`}
+          >
+            {isCamOn ? (
+              <Video className="size-4" />
+            ) : (
+              <VideoOff className="size-4" />
+            )}
+          </button>
+          <button
+            type="button"
+            className="btn btn-error btn-circle rounded-full"
+          >
+            <PhoneOff className="size-4" />
+          </button>
         </div>
       </div>
     </div>
