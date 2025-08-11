c4a0dc652ca54ffb1de5434d6edc4fae19318cef
Kent Adriane Goc-ong <kentadriane2@gmail.com>
2025-08-11 19:44:18 +0800

    fix selectedUser.id instead of get().selectedUser.id

--- src/store/useChatStore.js
+++ src/store/useChatStore.js
@@ -30,7 +30,7 @@ export const useChatStore = create((set, get) => ({
     console.log("Socket on new_message");
 
     socket.on("new_message", (newMessage) => {
-      if(selectedUser.id === newMessage.sender_id){
+      if(get().selectedUser.id === newMessage.sender_id){
         get().addMessageToChat(newMessage);
         get().playMusic()
       }
